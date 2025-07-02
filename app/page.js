424c8e7490b6652f40919163d9259a8a79ff3508
auilk <abdelghafour.lbaz01@gmail.com>
2025-07-02 23:52:16 +0100

    Initialize app with Next.js setup

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <h1 className="text-7xl font-black">VECTRA</h1>
+    </div>
+  );
+}
