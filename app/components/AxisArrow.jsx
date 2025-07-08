f69128032a70a3486ffd29f60046a0ec63f4735e
auilk <abdelghafour.lbaz01@gmail.com>
2025-07-08 03:31:55 +0100

    Refactor MoveGizmo and AxisArrow for cleaner and more robust code

--- app/components/AxisArrow.jsx
+++ app/components/AxisArrow.jsx
@@ -40,10 +40,16 @@ export default function AxisArrow({ direction , setX, setY})
 
         const HandleMouseMove = (event) =>
         {
-            if (isDragging)
+            const svgCanvasRect = elementRef.current.parentElement.parentElement.getBoundingClientRect();
+            const gizmoOriginRect = elementRef.current.parentElement.getBoundingClientRect();
+
+            if (direction === "x")
             {
-                if (direction === "x") setX(event.clientX - offset.current);
-                else setY(event.clientY - offset.current);
+                setX(event.clientX - svgCanvasRect.left - elements[selected.id].props.width / 2 - gizmoOriginRect.width / 2 - offset.current);
+            }
+            else
+            {
+                setY(event.clientY - svgCanvasRect.top - elements[selected.id].props.height / 2 + gizmoOriginRect.height / 2 + offset.current);
             }
         }
 
@@ -59,46 +65,30 @@ export default function AxisArrow({ direction , setX, setY})
         {
             window.removeEventListener("mousemove", HandleMouseMove);
             window.removeEventListener("mouseup", HandleMouseUp);
+            console.log("deleted");
         }
     }, [isDragging]);
     
     return (
         <div
             ref={elementRef}
-            className="relative select-none cursor-move"
+            className="absolute select-none"
             style={{
-                width: direction === "x" ? "55px" : "16px",
-                height: direction === "x" ? "16px" : "55px",
+                width: direction === "x" ? "80px" : "30px",
+                height: direction === "x" ? "30px" : "80px",
+                top: direction === "x" ? "0px" : "",
+                bottom : direction === "y" ? "100%" : "",
+                left: direction === "x" ? "100%" : "0%",
+                backgroundColor: direction === "x" ? "#FB2C36" : "#00C951",
+                cursor: direction === "x" ? "col-resize" : "row-resize"
             }}
-            onMouseDown={(event) => 
+            onMouseDown={(event) =>
                 {
-                    const rect = elementRef.current.parentElement.getBoundingClientRect();
-                    if (direction === "x") offset.current = event.clientX - rect.left + elements[selected.id]?.props?.width / 2;
-                    else if (direction === "y") offset.current = event.clientY - rect.top - rect.height + elements[selected.id]?.props?.height / 2 - 4;
+                    if (direction === "x") offset.current = event.clientX - event.target.getBoundingClientRect().left;
+                    else offset.current = event.target.getBoundingClientRect().height - event.clientY + event.target.getBoundingClientRect().top;
                     setIsDragging(true);
                 }
             }
-        >
-            <div
-                className="bg-white absolute left-0 top-1/2 -translate-y-1/2 rounded-bl-[2px]"
-                style={{
-                    width: direction === "x" ? "40px" : "8px",
-                    height: direction === "x" ? "8px" : "40px",
-                    top: direction === "x" ? "50%" : "0",
-                    translate: direction === "x" ? "0% -50%" : "50% 15px",
-                    borderBottomRightRadius: direction === "x" ? "0px" : "2px",
-                    borderTopLeftRadius: direction === "x" ? "2px" : "0px"
-                }}
-            ></div>
-
-            <div 
-                className="border-8 border-transparent border-l-white absolute left-10.5 top-0 scale-x-150"
-                style={{
-                    left: direction === "x" ? "44px" : "0px",
-                    top: direction === "x" ? "0px" : "-5px",
-                    rotate: direction === "x" ? "0deg" : "-90deg"
-                }}
-            ></div>
-        </div>
+        ></div>
     );
 }
--- app/components/MoveGizmo.jsx
+++ app/components/MoveGizmo.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useCallback, useRef, useState } from "react";
 import AxisArrow from "./AxisArrow";
 import { useSvgStore } from "../stores/svg-store";
 import { useShallow } from "zustand/react/shallow";
@@ -25,7 +24,7 @@ export default function MoveGizmo()
 
     const setX = (x) =>
     {
-        SetElement(selected.id, {...elements[selected.id].props, x: x});
+        SetElement(selected.id, {...elements[selected.id].props, x: x });
     }
 
     const setY = (y) =>
@@ -34,24 +33,15 @@ export default function MoveGizmo()
     }
 
     return (
-        <div
-            className="flex flex-col gap-1 absolute -translate-y-1/1 z-2"
+        <div 
+            className="w-[30px] h-[30px] bg-yellow-500 absolute -translate-1/2 z-1 cursor-move"
             style={{
-                top: `${elements[selected.id]?.props?.y + elements[selected.id]?.props?.height / 2 + 8}px`,
-                left: `${elements[selected.id]?.props?.x + elements[selected.id]?.props?.width / 2 - 8}px`,
+                top: `${elements[selected.id]?.props?.y + elements[selected.id]?.props.height / 2}px`,
+                left: `${elements[selected.id]?.props?.x + elements[selected.id]?.props.width / 2}px`,
             }}
         >
-            <div className="flex">
-                <AxisArrow direction="y" setY={setY}></AxisArrow>
-            </div>
-
-            <div className="flex gap-1">
-                <div className="w-4 h-4 border-2 border-white rounded-sm flex justify-center items-center">
-                    <div className="w-1 h-1 rounded-full bg-white"></div>
-                </div>
-
-                <AxisArrow direction="x" setX={setX}></AxisArrow>
-            </div>
+            <AxisArrow direction="x" setX={setX}></AxisArrow>
+            <AxisArrow direction="y" setY={setY}></AxisArrow>
         </div>
     );
 }
